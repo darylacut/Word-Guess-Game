7cf994887d9e3d1baa2168a9e47628bc99f4b258
Daryl Acut <darylacut@yahoo.com>
2019-07-07 07:12:58 -0400

    pseudocoded

---
+++ assets/javascript/game.js
@@ -0,0 +1,34 @@
+// game starts 
+    // score = 0
+    // game chooses random word from array of words
+    // game displays underlines corresponding to total number of letters of chosen word
+
+// player inputs random letter
+
+// if letter is typed before
+    // guesses remain unchanged
+
+// if letter is valid
+    // guesses  - 1
+    // replace corresponding underlines with corresponding letter
+
+// else 
+    // guesses -1 
+    // underlines remain the same
+
+// loop until
+    // all letters are guessed 
+            // score + 1
+            // word thought by computer is refreshed 
+            // underlines are refreshed
+
+    // or no guesses = 0
+            // score remains unchanged
+            // word though by computer is refreshed
+            // underlines refreshed
+
+
+            // loop until no more words in computer reserve. 
+
+var words = ["life", "strange", "choices"]
+var totalLetters = [""]
