3fa746dc4b53f965fb933b1512776905a46d7d62
Daryl Acut <darylacut@yahoo.com>
2019-07-07 16:16:10 -0400

    trying to display javascript to html elements

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,6 @@
-// variables for displaying to html
-var chosenWord = document.getElementById("chosenWord-text");
-var tries = document.getElementById("tries-text");
-var score = document.getElementById("score-text");
-var wordLetters = document.getElementById("wordLetters-text");
+debugger;
+
+
 
 alert("Welcome to the word guessing game!");
 
@@ -10,46 +8,57 @@ alert("Welcome to the word guessing game!");
 
 // variables declared
 // score = 0
-// tries = 0
+// tries = 10
 // game chooses random word from array of words
-
-var randomWord = ["life", "strange", "choices"];
-var tries = 0;
+var tries = 10;
 var score = 0;
 
-var chosenWord = randomWord[Math.floor(Math.random() * randomWord.length)];
 
+
+// variables for displaying to html
+var chosenWord = document.getElementById("chosenWord-text");
+var tries = document.getElementById("tries-text");
+var score = document.getElementById("score-text");
+var wordLetters = document.getElementById("wordLetters-text");
+
+var randomWord = ["life", "strange", "choices"];
+var chosenWord = randomWord[Math.floor(Math.random() * randomWord.length)];
+ 
 // game displays underlines corresponding to total number of letters of chosen word
 var wordLetters = [];
 for (var i = 0; i < chosenWord.length; i++) {
     console.log(wordLetters[i] = "_");
 }
- // to determine how many more blank letters left
+ // determine how many more blank letters left
 var blankLetters = chosenWord.length;
 
 // player inputs random letter
-document.onkeyup = function(event) {
+document.onkeyup = function () {
     var letterInput = event.key;
+
+// if letter is valid
+    // remaining tries - 1
+    // blank letters - 1
+    // replace corresponding underlines with corresponding letter
+// if letter has been typed before, guesses should remain unchanged
 
     for (var l = 0; l < chosenWord.Length; l++) {
         if (chosenWord[l] === letterInput) {
+           console.log (wordLetters[l] = letterInput);
+           tries--;
+           blankLetters--;
+        }
+
+        else {
+            tries--;
 
         }
     }
 
-
-// if letter has been typed before
-    
-
-    // guesses remain unchanged
-
-// if letter is valid
-    // guesses  - 1
-    // replace corresponding underlines with corresponding letter
-
+}
 
 // else 
-    // guesses -1 
+    // remaning tries -1 
     // underlines remain the same
 
 // loop until
@@ -60,8 +69,8 @@ document.onkeyup = function(event) {
 
     // or no guesses = 0
             // score remains unchanged
-            // word though by computer is refreshed
+            // word thought by computer is refreshed
             // underlines refreshed
 
 
-            // loop until no more words in computer reserve. 
+            // loop until no more words in computer reserve
